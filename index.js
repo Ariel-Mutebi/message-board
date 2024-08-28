a0f8235b6f6bf019d732e05a69b6895c145ef083
black-RAM <pplanes311@gmail.com>
2024-08-28 13:12:20 +0300

    typescript migration (for my OCD)

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-const path = require("node:path")
-const express = require("express")
-const dateFNS = require("date-fns")
-const { getMessages, addMessage, getMessage } = require("./models/messages")
-
-const app = express()
-const PORT = 8080
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const node_path_1 = __importDefault(require("node:path"));
+const express_1 = __importDefault(require("express"));
+const date_fns_1 = require("date-fns");
+const messages_1 = require("./models/messages");
+const app = (0, express_1.default)();
+const PORT = 8080;
 // set up view engine
-app.set("views", path.join(__dirname, "views"))
-app.set("view engine", "ejs")
-
+app.set("views", node_path_1.default.join(__dirname, "views"));
+app.set("view engine", "ejs");
 // middlewares
-app.use(express.static("public"))
-app.use(express.urlencoded({ extended: true }));
-
+app.use(express_1.default.static("public"));
+app.use(express_1.default.urlencoded({ extended: true }));
 // routes
-app.get("/", (_, res) => res.render("index", { messages: getMessages(), dateFormatter: dateFNS.format }))
-app.get("/new", (_, res) => res.render("newMessage"))
+app.get("/", (_, res) => res.render("index", { messages: (0, messages_1.getMessages)(), dateFormatter: date_fns_1.format }));
+app.get("/new", (_, res) => res.render("newMessage"));
 app.post("/new", (req, res) => {
-  addMessage({
-    text: req.body.text,
-    user: req.body.user,
-    added: new Date()
-  })
-
-  res.redirect("/")
-})
+    (0, messages_1.addMessage)({
+        text: String(req.body.text),
+        user: String(req.body.user),
+        added: new Date()
+    });
+    res.redirect("/");
+});
 app.get("/:messageId", (req, res) => {
-  const message = getMessage(req.params.messageId)
-  if (message) {
-    res.render("singleMessage", { message: message, dateFormatter: dateFNS.format })
-  } else {
-    res.render("404")
-  }
-})
-
+    const message = (0, messages_1.getMessage)(req.params.messageId);
+    if (message) {
+        res.render("singleMessage", { message: message, dateFormatter: date_fns_1.format });
+    }
+    else {
+        res.render("404");
+    }
+});
 // run
 app.listen(PORT, () => {
-  console.log("running on port: " + String(PORT))
-})
+    console.log("running on http://localhost:" + String(PORT));
+});
---
+++ index.ts
@@ -0,0 +1,41 @@
+import path from "node:path"
+import express from "express"
+import { format } from "date-fns"
+import { getMessages, addMessage, getMessage, uuidV1 } from "./models/messages"
+
+const app = express()
+const PORT = 8080
+
+// set up view engine
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+
+// middlewares
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
+
+// routes
+app.get("/", (_, res) => res.render("index", { messages: getMessages(), dateFormatter: format }))
+app.get("/new", (_, res) => res.render("newMessage"))
+app.post("/new", (req, res) => {
+  addMessage({
+    text: String(req.body.text),
+    user: String(req.body.user),
+    added: new Date()
+  })
+
+  res.redirect("/")
+})
+app.get("/:messageId", (req, res) => {
+  const message = getMessage(req.params.messageId as uuidV1)
+  if (message) {
+    res.render("singleMessage", { message: message, dateFormatter: format })
+  } else {
+    res.render("404")
+  }
+})
+
+// run
+app.listen(PORT, () => {
+  console.log("running on http://localhost:" + String(PORT))
+})
